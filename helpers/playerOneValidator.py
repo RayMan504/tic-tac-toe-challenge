aba00f5ebe4de60818dd66844e5f7f8fea1a4483
RayMan504 <uraeusmillet@gmail.com>
2025-10-20 14:40:08 -0500

    add pseudocode for tic tac toe move placement and win condition validators

---
+++ helpers/playerOneValidator.py
@@ -0,0 +1,6 @@
+# Validate Player 1: takes row and column position
+# check if position is occupied
+    # space occupied message
+    # trigger input again
+# place X on board
+
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # TODO: Generate three by three grid
+board = [["", "", ""], ["", "", ""], ["", "", ""]]
 # TODO: User can place X and 0 on grid
+row = input("Select Row")
+column = input("Select Column")
+board[int(row)][int(column)] = "X"
+# TODO: make sure board persists values
 # TODO: Track user input when placed on grid
 # TODO: Validate win conditions 
-
-msg = "testing source code!"
-
-print(msg)
+print(board)
