c1753c2399e59efc4f334b0a3520f649838f603c
RayMan504 <uraeusmillet@gmail.com>
2025-10-20 13:58:40 -0500

    initial step by step pseudocode

---
+++ main.py
@@ -0,0 +1,8 @@
+# TODO: Generate three by three grid
+# TODO: User can place X and 0 on grid
+# TODO: Track user input when placed on grid
+# TODO: Validate win conditions 
+
+msg = "testing source code!"
+
+print(msg)
